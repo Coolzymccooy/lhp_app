35f6577ef494c6332ccff06da3b40e06ababb651
Segun <coolshegz@gmail.com>
2026-05-30 02:12:57 +0100

    feat: add central site config to client

---
+++ client/src/content/site.ts
@@ -0,0 +1,25 @@
+export const site = {
+  name: 'The Lighthouse Church RCCG',
+  shortName: 'The Lighthouse Church',
+  locality: 'Bury, Manchester',
+  designation: 'Region 1 · Province 1 · Zone 2',
+  tagline: 'Reaching out, saving souls, making disciples through love',
+  address: 'The Rock Shopping Centre, Vue Cinema, The Rock, Bury, BL9 0ND',
+  phone: '+44 790 863 5374',
+  email: 'info@lighthousechurchburyrccg.co.uk',
+  services: [
+    { day: 'Sunday', name: 'Sunday Service', time: '10:00 AM' },
+    { day: 'Sunday', name: 'Thanksgiving Service', time: '10:00 AM', note: '1st Sunday' },
+    { day: 'Wednesday', name: 'Virtual Prayer Night', time: '5:30 PM' },
+    { day: 'Thursday', name: 'Digging Deep Word Study', time: '5:30 PM' },
+    { day: 'Friday', name: 'Virtual Vigil', time: '11:00 PM', note: 'Last Friday' },
+  ],
+  social: {
+    facebook: 'https://www.facebook.com/rccgtlp1',
+    instagram: 'https://www.instagram.com/rccgtlp1/',
+    youtube: 'https://www.youtube.com/@rccgtlp1',
+    x: 'https://x.com/rccgtlp1',
+  },
+} as const;
+
+export type ServiceTime = (typeof site.services)[number];
